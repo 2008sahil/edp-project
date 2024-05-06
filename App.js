b2ac6159500e042d458b7f970404ad9abf9cc2e3
Sahil Gupta <155609546+2008sahil@users.noreply.github.com>
2024-05-07 00:42:34 +0530

    corse policy added

--- App.js
+++ App.js
@@ -42,9 +42,9 @@ const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     
 });    
-app.get('/',(req,res)=>{
-    res.send("app is running")
-})
+// app.get('/',(req,res)=>{
+//     res.send("app is running")
+// })
 
 // Function to publish log to Redis
 function publishLog(log) {
